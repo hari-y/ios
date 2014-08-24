66607e3aafe8b7744fc22fd7da69b19f493aa57b
Hari Yalamanchili <yalamanchili.hari@gmail.com>
2014-08-24 11:15:19 -0600

    chapter1

---
+++ chapter1/Quiz/Quiz/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Quiz
+//
+//  Created by Hari  on 8/24/14.
+//  Copyright (c) 2014 Pensersoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ chapter1/Quiz/Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Hari  on 8/24/14.
+//  Copyright (c) 2014 Pensersoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
