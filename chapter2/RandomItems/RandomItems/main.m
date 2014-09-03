778513f7cd173029ce897208518dec15c5b16b48
Hari Yalamanchili <yalamanchili.hari@gmail.com>
2014-09-03 11:37:09 -0600

    chapter 2 changes

---
+++ chapter2/RandomItems/RandomItems/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Hari  on 9/3/14.
+//  Copyright (c) 2014 Pensersoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
