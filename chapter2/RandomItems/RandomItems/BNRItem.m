990055f55b624296fd68b35fbe89d5bb53cdce4d
Hari Yalamanchili <yalamanchili.hari@gmail.com>
2014-09-04 20:03:38 -0600

    chapter2

---
+++ chapter2/RandomItems/RandomItems/BNRItem.m
@@ -0,0 +1,73 @@
+//
+//  BNRItem.m
+//  RandomItems
+//
+//  Created by Hari  on 9/3/14.
+//  Copyright (c) 2014 Pensersoft. All rights reserved.
+//
+
+#import "BNRItem.h"
+
+@implementation BNRItem
+
+
+-(instancetype) initWithItemName:(NSString *)name
+                  valueInDollars:(int) value
+                    serialNumber:(NSString *) sNumber
+{
+    self=[super init];
+    if(self)
+    {
+        _itemName=name;
+        _serialNumber=sNumber;
+        _valueInDollars=value;
+        _dateCreated=[[NSDate alloc] init];
+    }
+    return self;
+}
+
+-(instancetype)
+-(void) setItemName:(NSString *) str
+{
+    _itemName=str;
+}
+
+-(NSString *) itemName
+{
+    return _itemName;
+}
+
+-(void) setSerialNumber:(NSString *) str
+{
+    _serialNumber=str;
+    
+}
+
+-(NSString *) serialNumber
+{
+    return _serialNumber;
+}
+
+
+-(void) setValueInDollars:(int)v
+{
+    _valueInDollars=v;
+}
+-(int) valueInDollars
+{
+    return _valueInDollars;
+}
+
+
+-(NSDate *) dateCreated
+{
+    return _dateCreated;
+}
+
+-(NSString *) description
+{
+    NSString *descriptionString=[[NSString alloc] initWithFormat:@"%@ (%@): worth $%d, recorded on %@",
+                                 self.itemName , self.serialNumber, self.valueInDollars,self.dateCreated];
+    return descriptionString;
+}
+@end
--- chapter2/RandomItems/RandomItems/main.m
+++ chapter2/RandomItems/RandomItems/main.m
@@ -7,15 +7,38 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BNRItem.h"
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSMutableArray *items=[[NSMutableArray alloc] init];
+        [items addObject:@"one"];
+        [items addObject:@"two"];
+        [items addObject:@"three"];
         
+        [items insertObject:@"Zero" atIndex:0];
+        
+        for(NSString *item in items){
+            NSLog(@"%@", item);
+        }
+        items=nil;
+        
+        int a=32;
+     
+        float b=89.99;
+        char c='A';
+        NSLog(@"Integer %d Float %f char: %c",a,b,c);
+        
+        BNRItem *item=[[BNRItem alloc] init];
+        
+        item.itemName=@"Red Sofa";
+        item.serialNumber=@"A1B2C";
+        item.valueInDollars=100;
+        NSLog(@"%@ ",item);
+        items=nil;
     }
     return 0;
 }
