c24759c694dc14e54fc4d8dc7f82c5a737e63a3a
Hari Yalamanchili <yalamanchili.hari@gmail.com>
2014-09-15 19:04:07 -0600

    chapter 4 hypnosis

--- chapter4/Hypnosister/BNRHypnosisView.m
+++ chapter4/Hypnosister/BNRHypnosisView.m
@@ -15,17 +15,43 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
+        self.backgroundColor=[UIColor clearColor];
+        
     }
     return self;
 }
 
-/*
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
     // Drawing code
+    
+    CGRect bounds=self.window.bounds;
+    CGPoint center;
+    center.x=(bounds.origin.x+bounds.size.width)/2.0;
+    center.y=(bounds.origin.y+bounds.size.height)/2.0;
+    
+    float maxRadius=hypot(bounds.size.width,bounds.size.height)/2.0;
+    
+    UIBezierPath *path=[[UIBezierPath alloc] init];
+    
+    for(float currentRadius=maxRadius;currentRadius>0;currentRadius-=20)
+    {
+        [path moveToPoint:CGPointMake(center.x+currentRadius,center.y)];
+        [path addArcWithCenter:center radius:currentRadius startAngle:0.0 endAngle:M_PI*2.0 clockwise:YES];
+    }
+    path.lineWidth=10;
+    [[UIColor lightGrayColor] setStroke];
+    [path stroke];
+    
+    //bronze challenge
+    
+    UIImage *logoImage=[UIImage imageNamed:@"logo.png"];
+    [logoImage set]
+    [logoImage drawInRect:bounds];
 }
-*/
+
 
 @end
