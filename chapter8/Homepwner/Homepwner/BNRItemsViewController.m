6a7bf5ec95a7e2628e75adf5a7e9329c1a3dc7b7
Hari Yalamanchili <yalamanchili.hari@gmail.com>
2014-10-19 17:18:32 -0600

    chapter 8 silver challenge

--- chapter8/Homepwner/Homepwner/BNRItemsViewController.m
+++ chapter8/Homepwner/Homepwner/BNRItemsViewController.m
@@ -36,11 +36,12 @@
     [super viewDidLoad];
     [self.tableView registerClass:[UITableViewCell class]
            forCellReuseIdentifier:@"UITableViewCell"];
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    UITableViewCell *footer = [[UITableViewCell alloc] init];
+    footer.textLabel.text = @"No more items!";
+    footer.rowHeight=44;
+    self.tableView.tableFooterView = footer;
+    self.tableView.rowHeight = 60;
+     footer.d
 }
 
 - (void)didReceiveMemoryWarning {
